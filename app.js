f7e124a1f98c8b9fcbcc2ca968a8d761c9c4f608
Dan Sulfaro <dsulfaro1@gmail.com>
2016-07-21 18:49:53 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,40 +1,26 @@
-    var string = "";
-    var showingDivs = 0;
-    var width = 100;
-    //$("#html").css("background-color", #808080);
-    // when clicking a button
-    $(".b").click(function(){
-      // if it is already clicked...
-      if($(this).hasClass("active")){
-        // remove active class to unselect it
-        $(this).removeClass("active");
-        // untoggle the container
-        string = (this.id);
-        $("#" + string + "Container").toggle();
-        // set codeContainers
-        showingDivs = showingDivs - 1;
-        var width = 100 / showingDivs;
-        $(".codeContainer").css("width", width + "%");
-      // if it is unclicked
-      } else {
-        // make it active
-        $(this).addClass("active");
-        // toggle the container
-        string = (this.id);
-        $("#" + string + "Container").toggle();
-        // set codeContainers
-        showingDivs = showingDivs + 1;
-        var width = 100 / showingDivs;
-        $(".codeContainer").css("width", width + "%");
-      }
+var app = angular.module('NPRTicker', []);
+
+app.controller('getNews', function($scope, $http, $interval) {
+  
+    $http.get("http://api.npr.org/query?id=1003&apiKey=MDI0NjcxNjQ2MDE0NjU0NDA0NTE0NDk2Nw000&format=json").then(function(response){
+      $scope.news = response.data.list.story;
+      $scope.headlines = [];
+      $scope.links = [];
+      for (item in $scope.news){
+        if ($scope.news[item].hasOwnProperty("title")){
+          $scope.headlines.push($scope.news[item].title.$text);
+          $scope.links.push($scope.news[item].link[0].$text);
+        }
+        
+        $scope.story_index = 0;
+        $interval(function(){
+          if($scope.story_index >= $scope.headlines.length){
+            $scope.story_index = 0;
+          }
+          else{
+            $scope.story_index++;
+          }
+        }, 3000);
+      }  
     });
-    // set the height of the codeContainers
-    var windowHeight = $(window).height();
-    var menuHeight = $("header").height();
-    var codeConHeight = windowHeight - menuHeight;
-    $(".codeContainer").height(codeConHeight + "px");
-    // run code
-    $("#run").click(function(){
-      $("iframe").contents().find("html").html('<style>' + $("#cssCode").val() + '</style>' + $("#htmlCode").val());
-    });
-    document.getElementById('result').contentWindow.eval( $('#jsCode').val() );
+});
